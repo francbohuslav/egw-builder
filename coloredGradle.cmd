7ad403a0fd89f5ab9a25a0131e04843ede820bdc
Bohuslav Franc <bohuslav.franc@unicornuniverse.eu>
2021-02-08 08:54:32 +0100

    Powersehll fix

--- coloredGradle.cmd
+++ coloredGradle.cmd
@@ -1,2 +1,2 @@
 @echo off
-gradlew --console plain start %2 %3 %4 %5 | pwsh %1\coloredGradle.ps1
+gradlew --console plain start %2 %3 %4 %5 | powershell -ExecutionPolicy Bypass -File %1\coloredGradle.ps1
