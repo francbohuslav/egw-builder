2834ac47dbd785269982c4aa4c5869fdb36fc534
Bohuslav Franc <bohuslav.franc@unicornuniverse.eu>
2021-04-06 10:06:16 +0200

    Do not short errors

--- coloredGradle.cmd
+++ coloredGradle.cmd
@@ -1,3 +1,3 @@
 @echo off
 gradlew --console plain start %4 %5 | node %1\coloredGradle.js %2 %3
-@REM type .\testColors.txt | node .\coloredGradle.js temp.log
+@REM type .\testColors.txt | node .\coloredGradle.js DG temp.log
