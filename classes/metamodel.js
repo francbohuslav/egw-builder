f40eb83467ff32c567156e6ab762807dbba2faf8
Bohuslav Franc <bohuslav.franc@unicornuniverse.eu>
2023-08-29 10:55:26 +0200

    Support metamodel 2.0

--- classes/metamodel.js
+++ classes/metamodel.js
@@ -6,30 +6,13 @@ class MetaModel {
    * @param {string} mainFolder
    * @param {IProject[]} projects
    * @param {IProject} project
+   * @param {boolean} isVersion11
    */
   async generateModel(mainFolder, projects, project, isVersion11) {
     await core.inLocationAsync(`${project.folder}/${project.server}/src/main/resources/config`, async () => {
       console.log(project.code);
-      const tempFile = "metamodel-1.0.new.json";
-      fs.copyFileSync("metamodel-1.0.json", tempFile);
-      const addProfiles = this.getAdditionalProfiles(mainFolder, projects, project, isVersion11);
-      const cmd = `egw-metamodel-generatorg01.cmd -p profiles.json${addProfiles} -m metamodel-1.0.new.json --mandatory-profiles Authorities Executives Auditors`;
-      //node C:\\Gateway\\_others\\egw_metamodelgeneratorg01\\egw_metamodelgeneratorg01\\cli.js
-      const code = await core.runCommand(cmd);
-      if (code.stdOut.indexOf("Profiles are not same !!!") > -1) {
-        console.log("in " + process.cwd());
-        console.log(project.code + ": " + cmd);
-        fs.unlinkSync(tempFile);
-        core.showError("Error during metamodel");
-      }
-      const data = core.readTextFile(tempFile).replace(/uu-energygateway.*?\//g, "");
-      core.writeTextFile(tempFile, data);
-      if (this.getNormalizedString("metamodel-1.0.json") === this.getNormalizedString(tempFile)) {
-        fs.unlinkSync(tempFile);
-      } else {
-        core.showMessage("Metamodel changed for " + project.code);
-        fs.renameSync(tempFile, "metamodel-1.0.json");
-      }
+      await this._generateMetamodelVersion(mainFolder, projects, project, isVersion11, 1);
+      await this._generateMetamodelVersion(mainFolder, projects, project, false, 2);
     });
   }
 
@@ -53,6 +36,37 @@ class MetaModel {
   getNormalizedString(file) {
     const data = fs.readFileSync(file, { encoding: "utf-8" }).toString();
     return data.replace(/\s+/g, "");
+  }
+
+  /**
+   * @param {string} mainFolder
+   * @param {IProject[]} projects
+   * @param {IProject} project
+   * @param {boolean} isVersion11
+   * @param {1|2} metamodelVersion
+   */
+  async _generateMetamodelVersion(mainFolder, projects, project, isVersion11, metamodelVersion) {
+    console.log("..." + metamodelVersion);
+    const tempFile = `metamodel-${metamodelVersion}.0.new.json`;
+    fs.copyFileSync(`metamodel-${metamodelVersion}.0.json`, tempFile);
+    const addProfiles = this.getAdditionalProfiles(mainFolder, projects, project, isVersion11);
+    const cmd = `egw-metamodel-generatorg01.cmd -p profiles.json${addProfiles} -m metamodel-${metamodelVersion}.0.new.json --mandatory-profiles Authorities Executives Auditors`;
+    //node C:\\Gateway\\_others\\egw_metamodelgeneratorg01\\egw_metamodelgeneratorg01\\cli.js
+    const code = await core.runCommand(cmd);
+    if (code.stdOut.indexOf("Profiles are not same !!!") > -1) {
+      console.log("in " + process.cwd());
+      console.log(project.code + ": " + cmd);
+      fs.unlinkSync(tempFile);
+      core.showError("Error during metamodel");
+    }
+    const data = core.readTextFile(tempFile).replace(/uu-energygateway.*?\//g, "");
+    core.writeTextFile(tempFile, data);
+    if (this.getNormalizedString(`metamodel-${metamodelVersion}.0.json`) === this.getNormalizedString(tempFile)) {
+      fs.unlinkSync(tempFile);
+    } else {
+      core.showMessage("Metamodel changed for " + project.code);
+      fs.renameSync(tempFile, `metamodel-${metamodelVersion}.0.json`);
+    }
   }
 }
 
